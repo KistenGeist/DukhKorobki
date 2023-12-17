9457e2a3485777744325db51b265448b5d9aa08c
Wladislaw Kusnezow <wladislawkusnezow@web.de>
2023-12-17 14:54:14 +0100

    created new path for services

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,7 +2,7 @@ import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HousingLocationComponent } from '../housing-location/housing-location.component';
 import { Housinglocation } from '../housinglocation';
-import { HousingService } from '../housing.service';
+import { HousingService } from '../Services/housing.service';
 
 @Component({
   selector: 'app-home',
