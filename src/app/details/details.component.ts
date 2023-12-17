6c7d201df6996dd9d4bab171eceb3076afb57d56
Wladislaw Kusnezow <wladislawkusnezow@web.de>
2023-12-17 17:20:41 +0100

    added detailpage html and css

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -13,10 +13,16 @@ import { Housinglocation } from '../housinglocation';
 })
 
 export class DetailsComponent {
+  // get route 
   route: ActivatedRoute = inject(ActivatedRoute);
+  // create variable of idParam
   housingLocationId: number = -1;
+  housingService: HousingService = inject(HousingService);
+  housingLocation!: Housinglocation | undefined;
   
   constructor() {
+    // get id param
     this.housingLocationId = Number(this.route.snapshot.params["id"]);
+    this.housingLocation = this.housingService.getHousingLocationById(this.housingLocationId);
   }
 }
