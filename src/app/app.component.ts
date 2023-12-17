47790c0703382f4e3e4a768c143f9d92480e6075
Wladislaw Kusnezow <wladislawkusnezow@web.de>
2023-12-17 15:24:22 +0100

    fixed error: routerModule was not imported correctly

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { HomeComponent } from './home/home.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, HomeComponent],
+  imports: [CommonModule, RouterOutlet, HomeComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
