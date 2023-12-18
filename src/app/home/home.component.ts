524ed4a64a6a2c0151567eb7f138611c9e5eac5b
Wladislaw Kusnezow <wladislawkusnezow@web.de>
2023-12-18 12:10:26 +0100

    added filter function

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,11 +15,24 @@ import { HousingService } from '../Services/housing.service';
 export class HomeComponent {
   // component global variables
   housingLocationList: Housinglocation[] = [];
+  filteredLocationList: Housinglocation[] = [];
   housingService: HousingService = inject(HousingService);
 
   // constructor function
   constructor() {
     this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
   }
 
+  filterResults(text: string){
+    if(!text) {
+      this.filteredLocationList = this.housingLocationList;
+    }
+    else{ 
+      this.filteredLocationList = this.housingLocationList.filter( x => x?.name.toLocaleLowerCase().includes(text.toLocaleLowerCase())
+      || x?.city.toLocaleLowerCase().includes(text.toLocaleLowerCase())
+      || x?.state.toLocaleLowerCase().includes(text.toLocaleLowerCase())
+      );
+    }
+  }
 }
